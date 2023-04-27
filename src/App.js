2f7f1f73dcd001954c09036c7cb0e325ae19362a
jeroxz12 <jeronimolago@hotmail.es>
2023-04-26 23:26:44 -0300

    Feat Carrousel

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from "./components/Header";
+import Home from "./pages/Home/Home.js";
+import Cities from "./pages/Cities/Cities.js";
+import Footer from "./components/Footer";
+import Hotels from "./pages/Hotels/Hotels";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cities" element={<Cities />}></Route>
+        <Route path="/hotels" element={<Hotels />}></Route>
+        <Route path="*" element={<Home />}></Route>
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+export default App;
