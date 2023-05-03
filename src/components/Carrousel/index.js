8a4fbb4004b77556ae65bc58eba78c6b52d1dd8c
Leo <leodesanmiguel@gmail.com>
2023-05-03 11:10:25 -0300

    Leo2: Carrousel

--- src/components/Carrousel/index.js
+++ src/components/Carrousel/index.js
@@ -1,75 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const items = [
-  {id:1,
-    titulo: "Tafí del Valle",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:2,
-    titulo: "Bariloche",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:3,
-    titulo: "New York",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:4,
-    titulo: "Bogotá",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:5,
-    titulo: "Rio de Janeiro",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:6,
-    titulo: "Quito",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:7,
-    titulo: "Texas",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:8,
-    titulo: "Miami",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:9,
-    titulo: "Lisboa",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:10,
-    titulo: "Bahia Blanca",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:11,
-    titulo: "Buenos Aires",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  },
-  {id:12,
-    titulo: "San Juan",
-    description: "",
-    imagenUrl: "https://wallpaperaccess.com/full/2680345.jpg",
-  }
-  
-];
+const baseURL = "http://localhost:3001/api/cities/all";
 
 const Carrousel = () => {
-  const getElements = (startIndex, finishIndex) => {
-    return items.slice(startIndex, finishIndex);
-  };
+  let [cities, setCities] = useState(null);
   let [indice, setIndice] = useState(0);
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => setCities(response.data.cities));
+  }, []);
+
+  if (!cities) return null;
+  //console.log(cities);
   return (
     <div
       id="carouselExampleControls"
@@ -78,45 +24,39 @@ const Carrousel = () => {
     >
       <div className="carousel-inner ">
         <div className="carousel-item active ">
-        {/* //   return <div className="card"  style={{width: "18rem",backgroundImage: "url("+item.imagenUrl+")",backgroundSize: "cover",
-        //   objectFit: "contain",
-        //   backgroundRepeat: "no-repeat",
-        //   backgroundPosition: "center"}}>
-        //   <div className="card-body" >
-        //     <h5 className="card-title">{item.titulo}</h5>
-        //     <a href="#" className="btn btn-primary btn-card">View more</a>
-        //   </div>
-        // </div> */}
-         <div className="card bg-dark text-white">
-        <img className="card-img" src={items[indice].imagenUrl} alt="Card image"/>
-        <div className="card-img-overlay  ">
-          <h5 className="card-title">{items[indice].titulo}</h5>
-          <p className="card-text">{items[indice].description}</p>
+          <div className="card bg-dark text-white">
+            <img
+              className="card-img"
+              src={cities[indice].imageUrl}
+              alt="Card image"
+            />
+            <div className="card-img-overlay  ">
+              <h5 className="card-title">{cities[indice].name}</h5>
+              <p className="card-text">{cities[indice].countryName}</p>
+            </div>
+          </div>
         </div>
       </div>
-          
-        </div>
-      </div> 
-      <a
+      <Link
         className="carousel-control-prev"
         href="#carouselExampleControls"
         role="button"
         data-slide="prev"
-        onClick={() => setIndice(indice>0?indice-1:items.length-1)}
+        onClick={() => setIndice(indice > 0 ? indice - 1 : cities.length - 1)}
       >
         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
         <span className="sr-only">Previous</span>
-      </a>
-      <a
+      </Link>
+      <Link
         className="carousel-control-next"
         href="#carouselExampleControls"
         role="button"
         data-slide="next"
-        onClick={() => setIndice(indice<11?indice+1:0)}
+        onClick={() => setIndice(indice < 11 ? indice + 1 : 0)}
       >
         <span className="carousel-control-next-icon" aria-hidden="true"></span>
         <span className="sr-only">Next</span>
-      </a>
+      </Link>
     </div>
   );
 };
